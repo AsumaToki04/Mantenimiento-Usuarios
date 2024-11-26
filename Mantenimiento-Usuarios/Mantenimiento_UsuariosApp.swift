930d5f483c4136df67763c694fa4fc9bc60999f1
AsumaToki04 <megamanzxu@hotmail.com>
2024-11-25 19:56:37 -0500

    Initial Commit

---
+++ Mantenimiento-Usuarios/Mantenimiento_UsuariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mantenimiento_UsuariosApp.swift
+//  Mantenimiento-Usuarios
+//
+//  Created by DAMII on 25/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Mantenimiento_UsuariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
